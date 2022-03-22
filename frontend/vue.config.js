0a7a2636574d65853bde078d6e3671a5dfda3bd2
deekor666 <deekor666@gmail.com>
2022-03-23 02:44:12 +0300

    troubles with connect to frontend container

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,8 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-    lintOnSave: false,
     transpileDependencies: true,
     devServer: {
-        open: true,
-        port: 8080,
-        https: false,
+        compress: true,
+        port: '8080',
     },
 })
