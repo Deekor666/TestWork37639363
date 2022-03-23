5f4226a7c972efcf206c8f579543cb7c58bfba1c
deekor666 <deekor666@gmail.com>
2022-03-23 11:58:06 +0300

    fix nginx config

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,11 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get(
-    '/',
+    '/api',
     function () {
         return view('welcome');
     }
 );
-Route::get('{any}', function () {
-    return view('layouts.app');
-})->where('any', '.*');
